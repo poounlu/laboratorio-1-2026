96712bd66210f7f396246b594ce576d2a7acb01f
francotonini <francotoninisilva@gmail.com>
2026-09-01 20:47:58 -0300

    pusheado

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,40 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private int poder;
+    private String tipode;
+
+    public Artefacto (String nombre, int poder, String tipode){
+        this.nombre = nombre;
+        this.poder=poder;
+        this.tipode= tipode;
+
+    }
+    public void setArtefacto (String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    @Override
+    public boolean equals (Object o){
+        if (this == o )
+            return  true;
+        if(o==null || getClass() !=o.getClass())
+            return  false;
+
+    Artefacto artefacto = (Artefacto) o;
+        return Object.equals(nombre, artefacto.nombre);
+        return false;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(nombre);
+    }
 }
+
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,40 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+
+
+    public void sinDuplicados(){
+        Set<Artefacto> artefactos = new HashSet<>() {
+
+
+;        Artefacto artefacto1 = new Artefacto();
+        String nombre = Artefacto.getNombre();
+    };
+
+
+    void buscarArtefactoPorTipo(){
+        list<Artefacto> misArtefactos = new Arraylist<>();
+
+
+        }
+
+
+
+
+    public contarArtefactoPorTipo (){
+        map<String, Interger> artefactoPorTipo = new HashMap<>();
+        int contador =0;
+
+
+        int conteoActual = artefactoPorTipo.get("Amuleto");
+        artefactoPorTipo.put ("Amuleto", conteoActual + 1);
+
+        System.out.println(artefactoPorTipo.get("Amuleto"));
+    }
+
+    List<Artefacto> misArtefacto = new ArrayList<>();
+
+
 }
