bea511075ee3992b60c102ed1e852213c17e1dec
sanntiism <santinomorrone05@gmail.com>
2026-09-01 20:59:51 -0300

    Entrega Grupo Santino - Mateo

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,44 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import ar.edu.unlu.poo.labo1.libreria.Libro;
+
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private Integer poder;
+    private String tipo;
+
+    public Artefacto(String nombre, Integer poder, String tipo) {
+        this.nombre = nombre;
+        this.poder = poder;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getPoder() {
+        return poder;
+    }
+
+    public String getTipo() {return tipo;}
+
+    @Override
+    public boolean equals(Object o) {
+        // 1. ¿Somos el mismo objeto en memoria?
+        if (this == o) return true;
+        // 2. ¿Es nulo o de una clase diferente?
+        if (o == null || getClass() != o.getClass()) return false;
+        // 3. Convertimos el objeto y comparamos los campos clave
+        Artefacto artefacto = (Artefacto) o;
+        return Objects.equals(nombre, artefacto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        // 4. Generamos un hash code a partir de los mismos campos
+        return Objects.hash(nombre);
+    }
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,38 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    Set<Artefacto> artefactos = new HashSet<>();
+
+    Map<String, List<Artefacto>> tipos  = new HashMap<>();
+
+    void agregarArtefacto(Artefacto artefacto){
+
+        artefactos.add(artefacto);
+    }
+
+
+    public Set obtenerArtefactosUnicos(){
+        return artefactos;
+    }
+
+    public List buscarArtefactosPorTipo(String tipo){
+        List<Artefacto> lista = new ArrayList<>();
+
+
+        return lista;
+    }
+
+    Map<String, Integer> contarArtefactosPorTipo(){
+
+
+        return null;
+
+    }
+
+    Artefacto obtenerArtefactoMasPoderoso(){
+
+        return null;
+    }
 }
