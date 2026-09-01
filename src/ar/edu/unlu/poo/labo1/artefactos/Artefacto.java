aebe8948b3faf1562a7405dadef7d4359fa83cd9
celestecamp <celestecampm@gmail.com>
2026-09-01 20:45:07 -0300

    .

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,54 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private int poder;
+    private String tipo;
+
+    public Artefacto (String nombre, int poder, String tipo) {
+        this.nombre = nombre;
+        this.poder = poder;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // 1. ¿Somos el mismo objeto en memoria?
+        if (this == o) return true;
+        // 2. ¿Es nulo o de una clase diferente?
+        if (o == null || getClass() != o.getClass()) return false;
+        // 3. Convertimos el objeto y comparamos los campos clave
+        Artefacto artefacto = (Artefacto) o;
+        return Objects.equals(nombre, artefacto.nombre) &&
+                Objects.equals(tipo, artefacto.tipo) &&
+                Objects.equals(poder, artefacto.poder);
+
+    }
+
+    @Override
+    public int hashCode() {
+        // 4. Generamos un hash code a partir de los mismos campos
+        return Objects.hash(nombre, tipo, poder);
+    }
 }
+
+
+/*Clase Artefacto:
+Atributos: nombre (String), poder (int), tipo (¿String?).
+Constructor para inicializar todos los atributos.
+Métodos getters para todos sus atributos.
+Implementación correcta de equals() y hashCode(): dos artefactos se consideran iguales si tienen el mismo nombre.*/
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,69 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    private Set<Artefacto> artefactos;
+
+    public CatalogoArtefactos() {
+        this.artefactos = new HashSet<>();
+    }
+
+    public void agregarArtefacto (Artefacto artefacto) {
+        artefactos.add(artefacto);
+    }
+
+    public Set obtenerArtefactosUnicos() {
+        return artefactos;
+    }
+
+    public List buscarArtefactosPorTipo (String tipo) {
+        List<Artefacto> lista = new ArrayList<>();
+
+        for (Artefacto a : artefactos) {
+            if (a.getTipo().equals(tipo)) {
+                lista.add(a);
+            }
+        }
+
+        return lista;
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo() {
+        Map<String, Integer> artefactosPorTipo = new HashMap<>();
+
+        for (Artefacto a : artefactos) {
+            if (!artefactosPorTipo.containsKey(a.getTipo())) {
+                artefactosPorTipo.put(a.getTipo(), 1);
+            }
+            else {
+                artefactosPorTipo.put(a.getTipo(), (artefactosPorTipo.get(a.getTipo()) + 1));
+            }
+        }
+
+        return artefactosPorTipo;
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso() {
+        if (artefactos.isEmpty()) return null;
+
+        int poderMax = 0;
+        for (Artefacto a : artefactos) {
+            if (a.getPoder() > poderMax) {
+                poderMax = a.getPoder();
+            }
+        }
+
+        Artefacto artefacto;
+        for (Artefacto a : artefactos) {
+            if (a.getPoder() == poderMax) {
+                artefacto = a;
+            }
+        }
+
+        return artefacto;
+    }
 }
+
+/*Clase CatalogoArtefactos:
+Artefacto obtenerArtefactoMasPoderoso(): Devuelve el artefacto con el mayor nivel de poder. Si hay varios con el mismo poder máximo, puede devolver cualquiera de ellos. Si el catálogo está vacío, debe devolver null.*/
