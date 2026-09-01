46b34a88a9d223dbd849c14f1bd0b0f064536dbf
Agustin327 <agustin327@outlook.com.ar>
2026-09-01 20:45:15 -0300

    Entrega

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,53 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import ar.edu.unlu.poo.labo1.libreria.Libro;
+
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombreArtefacto;
+    private int nivelPoder;
+    private String tipoArtefacto;
+
+    public Artefacto(String nombreArtefacto, int nivelPoder, String tipoArtefacto){
+        this.nombreArtefacto = nombreArtefacto;
+        this.nivelPoder = nivelPoder;
+        this.tipoArtefacto = tipoArtefacto;
+    }
+
+    public int getNivelPoder() {
+        return nivelPoder;
+    }
+
+    public String getNombreArtefacto() {
+        return nombreArtefacto;
+    }
+
+    public String getTipoArtefacto() {
+        return tipoArtefacto;
+    }
+
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artefacto artefacto = (Artefacto) o;
+        return Objects.equals(nombreArtefacto, artefacto.nombreArtefacto);
+    }
+
+    public int hashCode() {
+
+        return Objects.hash(nombreArtefacto, nivelPoder, tipoArtefacto);
+    }
+
+    public String getNombre() {
+        return nombreArtefacto;
+    }
+
+    public int getPoder() {
+        return nivelPoder;
+    }
+}
+
+
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,35 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    private HashSet<Artefacto> setArtefactos;
+    private Map<Artefacto, Integer>  mapArtefactos;
+    private ArrayList<Artefacto> arrayArtefactos ;
+
+
+    public CatalogoArtefactos (){
+        this.setArtefactos = new HashSet<>();
+        this.mapArtefactos = new HashMap<>();
+        this.arrayArtefactos = new ArrayList<>();
+    }
+
+
+    public void agregarArtefacto(Artefacto unArtefacto) {
+        setArtefactos.add(unArtefacto);
+
+
+    }
+
+    public Map<Object, Object> obtenerArtefactosUnicos() {
+    }
+
+    public List<Artefacto> buscarArtefactosPorTipo(String amuleto) {
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo() {
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso() {
+    }
 }
