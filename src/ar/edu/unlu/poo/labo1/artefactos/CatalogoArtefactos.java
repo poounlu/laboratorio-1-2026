9b769ec5c0316a462378f8e986e5d26009bad4b2
Santino Mataza <santinomataza@gmail.com>
2026-09-01 21:03:18 -0300

    Entrega Laboratorio 1. Clase

--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,38 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+    private Set<Artefacto> almacen = new HashSet<>();
+
+
+    public CatalogoArtefactos(Artefacto artefacto) {
+
+    }
+
+    public void agregarArtefacto(Artefacto artefacto){
+        almacen.add(artefacto);
+    }
+
+    public Set<Artefacto> obtenerArtefactosUnicos(){
+        return almacen;
+    }
+
+    public List<Artefacto> buscarArtefactosPorTipo(String tipo){
+        List<Artefacto> misArtefactos = new ArrayList<>();
+
+        for (Artefacto a : almacen){
+            if (a.getTipo().equals(tipo)) misArtefactos.add();
+        }
+
+        return null;
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo(){
+        return null;
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso(){
+        return null;
+    }
 }
